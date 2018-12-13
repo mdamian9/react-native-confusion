932850eef2a15ebc2e4781a7d1dfa534be2ffaa7
mdamian9 <m.damian9@hotmail.com>
2018-12-13 13:13:35 -0800

    Modified components to become class components. Added new view to app to render Detailed Dish

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 import { DISHES } from '../shared/dishes';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        };
-    };
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail }
+},
+    {
+        initialRouteName: 'Menu',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"
+            }
+        }
+    }
+);
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId })
-    };
+class Main extends Component {
 
     render() {
         return (
-            <View>
-                <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-            </View>
-        );
+            <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <MenuNavigator />
+            </View>);
     };
 };
 
