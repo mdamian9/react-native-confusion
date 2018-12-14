f5e488dc356176bac68a69d9f2567280f67a9176
mdamian9 <m.damian9@hotmail.com>
2018-12-14 14:13:49 -0800

    Small adjustment to AboutComponent

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,17 @@ import { ListItem } from 'react-native-elements';
 import { Card } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
 
+const History = (props) => {
+    return (
+        <Card title="Our History">
+            <Text>
+                Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.{"\n\n"}
+                The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
+            </Text>
+        </Card>
+    );
+};
+
 class About extends Component {
 
     constructor(props) {
@@ -14,21 +25,10 @@ class About extends Component {
     };
 
     static navigationOptions = {
-        title: 'Our History'
+        title: 'About Us'
     };
 
     render() {
-
-        const History = () => {
-            return (
-                <Card title="Our History">
-                    <Text>
-                        Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.{"\n\n"}
-                        The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
-                    </Text>
-                </Card>
-            );
-        };
 
         const renderLeaderItem = ({ item, index }) => {
             return (
