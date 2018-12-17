7d8dbea4b6c351173e07248f8d6cc1d519f8aec5
mdamian9 <m.damian9@hotmail.com>
2018-12-17 01:02:56 -0800

    Connecting / configuring redux - modified AboutComponent & DishdetailComponent

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,14 @@ import { Text, View, ScrollView, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { Card } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        leaders: state.leaders
+    };
+};
 
 const History = (props) => {
     return (
@@ -17,13 +25,6 @@ const History = (props) => {
 
 class About extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            leaders: LEADERS
-        };
-    };
-
     static navigationOptions = {
         title: 'About Us'
     };
@@ -37,7 +38,7 @@ class About extends Component {
                     title={item.name}
                     subtitle={item.description}
                     hideChevron={true}
-                    leftAvatar={{ source: require('./images/alberto.png') }}
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
                 />
             );
         };
@@ -47,7 +48,7 @@ class About extends Component {
                 <History />
                 <Card title="Corporate Leadership">
                     <FlatList
-                        data={this.state.leaders}
+                        data={this.props.leaders.leaders}
                         renderItem={renderLeaderItem}
                         keyExtractor={item => item.id.toString()}
                     />
@@ -58,4 +59,4 @@ class About extends Component {
     };
 };
 
-export default About;
+export default connect(mapStateToProps)(About);
