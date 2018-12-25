617f96aec6c248e3a3dcd68c4e2b234bb8174a89
mdamian9 <m.damian9@hotmail.com>
2018-12-24 23:23:33 -0800

    Used react-native-animatable to add animation to components

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 const Contact = () => {
     return (
         <View>
-            <Card title="Contact Information">
-                <Text>
-                    121, Clear Water Bay Road{'\n\n'}
-                    Clear Water Bay, Kowloon{'\n\n'}
-                    HONG KONG{'\n\n'}
-                    Tel: +852 1234 5678{'\n\n'}
-                    Fax: +852 8765 432{'\n\n'}
-                    Email: confusion@food.net
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                <Card title="Contact Information">
+                    <Text>
+                        121, Clear Water Bay Road{'\n\n'}
+                        Clear Water Bay, Kowloon{'\n\n'}
+                        HONG KONG{'\n\n'}
+                        Tel: +852 1234 5678{'\n\n'}
+                        Fax: +852 8765 432{'\n\n'}
+                        Email: confusion@food.net
                 </Text>
-            </Card>
+                </Card>
+            </Animatable.View>
         </View>
     );
 };
