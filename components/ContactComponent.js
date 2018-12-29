85f6f4f50e044b4719190ac33dadf723515b18cb
mdamian9 <m.damian9@hotmail.com>
2018-12-28 16:35:32 -0800

    Used Expo SDK MailComposer to enable app to send emails using native email app

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, ScrollView } from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import { MailComposer } from 'expo';
 
-const Contact = () => {
-    return (
-        <View>
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
-                <Card title="Contact Information">
-                    <Text>
-                        121, Clear Water Bay Road{'\n\n'}
-                        Clear Water Bay, Kowloon{'\n\n'}
-                        HONG KONG{'\n\n'}
-                        Tel: +852 1234 5678{'\n\n'}
-                        Fax: +852 8765 432{'\n\n'}
-                        Email: confusion@food.net
-                </Text>
-                </Card>
-            </Animatable.View>
-        </View>
-    );
+class Contact extends Component {
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        });
+    };
+
+    render() {
+        return (
+            <ScrollView>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                    <Card title="Contact Information">
+                        <Text>
+                            121, Clear Water Bay Road{'\n\n'}
+                            Clear Water Bay, Kowloon{'\n\n'}
+                            HONG KONG{'\n\n'}
+                            Tel: +852 1234 5678{'\n\n'}
+                            Fax: +852 8765 432{'\n\n'}
+                            Email: confusion@food.net{'\n'}
+                        </Text>
+                        <Button
+                            title="Send Email"
+                            buttonStyle={{ backgroundColor: "#512DA8" }}
+                            icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                            onPress={this.sendMail}
+                        />
+                    </Card>
+                </Animatable.View>
+            </ScrollView>
+        );
+    };
 };
 
 export default Contact;
